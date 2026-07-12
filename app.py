1b0bfb1fc775d81afb2d732301f357173faa33ae
Dhanya J Nadig <166634241+DJN27@users.noreply.github.com>
2026-07-13 01:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 import google.generativeai as genai
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 st.set_page_config(
